a918e0915a7f627fc6121095ad65bb02cceab823
Bandito <49611158+Suppplier@users.noreply.github.com>
2021-06-27 18:47:41 +0900

    0627-검색폼 3

--- practice-react/1-vanilla/js/views/SearchFormView.js
+++ practice-react/1-vanilla/js/views/SearchFormView.js
@@ -1,4 +1,4 @@
-import { qs } from "../helpers.js";
+import { qs, on } from "../helpers.js";
 import View from "./View.js";
 
 const tag = "[SearchFormView]";
@@ -10,12 +10,32 @@ export default class SearchFormView extends View {
     super(qs("#search-form-view"));
 
     this.resetElement = qs("[type=reset]", this.element);
+    this.inputElement = qs("[type=text]", this.element);
+
 
     this.showResetButton(false);
+    this.bindEvent();
     // TODO
   }
 
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvent(){
+    on(this.inputElement, "keyup", () => this.handleKeyup())
+    on(this.element, "submit", event => this.handleSubmit(event))
+  }
+
+  handleKeyup() {
+    const { value } = this.inputElement;
+    this.showResetButton(value.length > 0);
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    console.log(tag, "handlesubmit");
+    const { value } = this.inputElement;
+    this.emit("@submit", {value});
   }
 }
