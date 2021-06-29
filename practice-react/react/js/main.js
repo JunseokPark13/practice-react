2667f28bf22f3f33470a483396532da3913d17c0
Bandito <49611158+Suppplier@users.noreply.github.com>
2021-06-29 21:42:02 +0900

    0629 - React 검색 폼 1

--- practice-react/react/js/main.js
+++ practice-react/react/js/main.js
@@ -1,15 +1,38 @@
-const element1 = (
-  <>
-    <header>
-      <h2 className="container">검색</h2>
-    </header>
-   <div className="container">
-      <form>
-        <input type="text" placeholder="검색어를 입력하세요" autoFocus />
-        <button type="reset" className="btn-reset"></button>
-      </form>
-   </div>
-  </>
-)
+class App extends React.Component {
+  constructor() {
+    super();
 
-ReactDOM.render(element1, document.querySelector("#app"));
+    this.state = {
+      searchKeyword: "",
+    };
+  }
+
+  handleChangeInput(event) {
+    const searchKeyword = event.target.value;
+    this.setState({ searchKeyword });
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <h2 className="container">검색</h2>
+        </header>
+        <div className="container">
+          <form>
+            <input
+              type="text"
+              placeholder="검색어를 입력하세요"
+              autoFocus
+              value={this.state.searchKeyword}
+              onChange={(event) => this.handleChangeInput(event)}
+            />
+            <button type="reset" className="btn-reset"></button>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
