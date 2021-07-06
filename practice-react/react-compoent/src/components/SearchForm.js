c80a74ff49b4c6b27b72d16677a83968d55c45c4
Bandito <49611158+Suppplier@users.noreply.github.com>
2021-07-06 23:10:08 +0900

    0706 - React component 검색 폼

---
+++ practice-react/react-compoent/src/components/SearchForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const SearchForm = ({value, onChange, onSubmit, onReset}) => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("SearchForm onSubmit")
+        onSubmit(value)
+    }
+  
+    const handleReset = () => {
+        onReset()
+    }
+
+    const handleChangeInput = (event) => {
+        onChange(event.target.value)
+    }
+
+    return (
+        <form
+        onSubmit={handleSubmit}
+        onReset={handleReset}
+        >
+          <input
+            type="text"
+            placeholder="검색어를 입력하세요"
+            autoFocus
+            value={value}
+            onChange={handleChangeInput}
+          />
+          {value.length > 0 && (
+            <button type="reset" className="btn-reset"></button>
+          )}
+        </form>
+      );
+}
+
+export default SearchForm
